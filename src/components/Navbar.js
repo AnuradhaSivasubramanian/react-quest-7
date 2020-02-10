5ff24405c6577c226ab288260a6f694262a56aec
anu radha <anu.sivasubramanian@gmail.com>
2020-02-10 17:09:40 +0100

    my react 7 quest

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <h1>Wikipedia Page of Simpsons</h1>
+    </nav>
+  );
+}
+export default Navbar;
